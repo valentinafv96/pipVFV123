e82ff9ed86b392f8801019647d5d1dcdfe88a749
valentinafv96 <64854217+valentinafv96@users.noreply.github.com>
2022-09-08 11:20:31 -0500

    Create pipVFV123.py

---
+++ desoper/pipVFV123.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+r'''
+Hello module
+'''
+
+
+def hello():
+    r'''
+    Hello function
+    '''
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    r'''
+    Hello main
+    '''
+    hello()
