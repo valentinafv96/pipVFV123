c62ea35b817a4a74bb6b6cb09ba62f3f668afc51
valentinafv96 <64854217+valentinafv96@users.noreply.github.com>
2022-09-08 11:07:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,14 +34,14 @@ if os.name in ('nt', 'dos'):
 def main():
     setup(
         # Application name:
-        name="desoper",
+        name="pipVFV123",
 
         # Version number (initial):
-        version="0.0.3",
+        version="0.0.1",
 
         # Application author details:
-        author="restrepo",
-        author_email="restrepo@udea.edu.co",
+        author="franco",
+        author_email="valentina.franco@udea.edu.co",
 
         # Packages
         packages=find_packages(exclude=['tests']),
@@ -50,7 +50,7 @@ def main():
         include_package_data=True,
 
         # Details
-        url="https://github.com/restrepo/DevOps",
+        url="https://github.com/valentinafv96/pipVFV123",
 
         license="BSD",
 
